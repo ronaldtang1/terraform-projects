a0e18e86651c29a1fad52dbcff66b78b26e8accd
Ronald Tang <ronaldtang12@gmail.com>
2023-01-24 00:54:13 -0500

    added rds file

--- variable.tf
+++ variable.tf
@@ -47,3 +47,28 @@ variable "ssh_location" {
   description   = "the ip address that can ssh into the ec2 instance"
   type          = string
 }
+
+# rds variables
+variable "database_snapshot_identifier" {
+  default       = "arn:aws:rds:us-east-1:451807425749:snapshot:dev-rds-db-snapshot"
+  description   = "the database snapshot arn"
+  type          = string
+}
+
+variable "database_instance_class" {
+  default       = "db.t2.micro"
+  description   = "the database instance type"
+  type          = string
+}
+
+variable "database_instance_identifier" {
+  default       = "dev-rds-db"
+  description   = "the database instance identifier"
+  type          = string
+}
+
+variable "multi_az_deployment" {
+  default       = false
+  description   = "create standby db instance"
+  type          = bool
+}
